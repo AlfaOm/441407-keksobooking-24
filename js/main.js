8c5a47678bccdcf37a9f850f1cf34c1923c3df8f
AlFaOm <al_fa_omega@mail.ru>
2021-10-10 00:05:34 +0700

    Исправления

--- js/main.js
+++ js/main.js
@@ -22,29 +22,13 @@ const getRandomFloat = function (min, max, fraction = 0) {
 getRandomFloat(0, 100, 12);
 */
 
-
 // дз 4.9
 
-const AVATAR = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-];
 const TITLE = [
   'Аренда квартиры', 'Аренда дома', 'Аренда комнаты',
 ];
-const ADDRESS = [
-  '{{location.lat}}', '{{location.lng}}',
-];
-const PRICE = [
-  100, 15000,
-];
 const TYPE = [
   'palace', 'flat', 'house', 'bungalow', 'hotel',
-];
-const ROOMS = [
-  1, 5,
-];
-const GUESTS = [
-  1, 5,
 ];
 const CHECKIN = [
   '12:00', '13:00', '14:00',
@@ -61,75 +45,64 @@ const DESCRIPTION = [
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-const LAT = [
-  35.65000, 35.70000,
-];
-const LNG = [
-  139.70000, 139.80000,
-];
 
-
-// author
-const getRandomArrayAuthor = function(elements) {
-  return elements[_.random(0, elements.length - 1)];
-};
-
-const createAuthor = function() {
-  return {
-    avatar: `img/avatars/user${getRandomArrayAuthor(AVATAR)}.png`,
-  };
-};
-
-const similarAnnouncementAuthor = Array.from({length: 10}, createAuthor);
-similarAnnouncementAuthor(); //во избежание ошибок
-// console.log(similarAnnouncementAuthor);
-
-
-// offer
-const getRandomArrayOffer = function(elements) {
-  return elements[_.random(0, elements.length - 1)];
-};
-
-// price
-const getRandomPrice = function (min, max) {
+// Функция, возвращающая случайное целое число
+const getRandomInteger = function (min, max) {
   const rand = Math.abs(min + Math.random() * (max + 1 - min));
   return Math.round(rand);
 };
-getRandomPrice();
 
-const createOffer = function() {
-  return {
-    title: getRandomArrayOffer(TITLE),
-    address: getRandomArrayOffer(ADDRESS),
-    price: getRandomArrayOffer(getRandomPrice(PRICE)),
-    type: getRandomArrayOffer(TYPE),
-    rooms: getRandomArrayOffer(getRandomPrice(ROOMS)),
-    guests: getRandomArrayOffer(getRandomPrice(GUESTS)),
-    checkin: getRandomArrayOffer(CHECKIN),
-    checkout: getRandomArrayOffer(CHECKOUT),
-    features: getRandomArrayOffer(FEATURES),
-    description: getRandomArrayOffer(DESCRIPTION),
-    photos: getRandomArrayOffer(PHOTOS),
-  };
+// Функция, возвращающая случайное число с плавающей точкой
+const getRandomFloat = function (min, max, fraction = 1) {
+  const fractionDegree = 10 ** fraction;
+  const result = ((Math.abs(Math.random() * (max - min) + min) * fractionDegree) / fractionDegree);
+  return result.toFixed(fraction);
 };
 
-const similarAnnouncementOffer = Array.from({ length: 10 }, createOffer);
-similarAnnouncementOffer(); //во избежание ошибок
-// console.log(similarAnnouncementOffer);
-
-
-// location
-const getRandomArrayLocation = function(elements) {
+const getRandomArrayElement = function (elements) {
   return elements[_.random(0, elements.length - 1)];
 };
 
-const getRandomLocation = function() {
+const createAuthor = function (index) {
   return {
-    lat: getRandomArrayLocation(LAT),
-    lng: getRandomArrayLocation(LNG),
+    avatar: `img/avatars/user${index}.png`,
   };
 };
 
-const similarAnnouncementLocation = Array.from({ length: 10 }, getRandomLocation);
-similarAnnouncementLocation(); //во избежание ошибок
-// console.log(similarAnnouncementLocation);
+const getRandomLocation = function () {
+  return {
+    lat: getRandomFloat(35.65000, 35.70000, 5),
+    lng: getRandomFloat(139.70000, 139.80000, 5),
+  };
+};
+
+const createOffer = function () {
+  return {
+    title: getRandomArrayElement(TITLE),
+    address: getRandomLocation(),
+    price: getRandomInteger(100, 1500),
+    type: getRandomArrayElement(TYPE),
+    rooms: getRandomInteger(1, 5),
+    guests: getRandomInteger(1, 5),
+    checkin: getRandomArrayElement(CHECKIN),
+    checkout: getRandomArrayElement(CHECKOUT),
+    features: getRandomArrayElement(FEATURES),
+    description: getRandomArrayElement(DESCRIPTION),
+    photos: getRandomArrayElement(PHOTOS),
+  };
+};
+
+
+const createAd = function (index) {
+  return {
+    author: createAuthor(index + 1),
+    offer: createOffer(),
+    location: getRandomLocation(),
+  };
+};
+
+
+const mockData = new Array(10).fill('').map((__, index) => createAd(index));
+
+mockData;
+console.log(mockData);
